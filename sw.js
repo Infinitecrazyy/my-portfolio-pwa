21746c15a2b48d1c7d38e839ba9bfc839d331804
Aman Jha <amanom0707@gmail.com>
2026-01-12 18:06:41 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const cacheName = 'portfolio-v1';
+const assets = [
+  './',
+  './index.html',
+  './icon.png'
+];
+
+// Install event: Caches the files
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Fetch event: Serve from cache if offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
